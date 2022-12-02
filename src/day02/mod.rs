27a8d0517b6255a66e96acaa4946bb4d67916f2e
Rafq77 <sokol-77@wp.pl>
2022-12-02 15:01:17 +0100

    Day02

---
+++ src/day02/mod.rs
@@ -0,0 +1,97 @@
+use std::collections::HashMap;
+
+// A/X - Rock
+// B/Y - Paper
+// C/Z - Scissors
+
+// 1 Rock
+// 2 Paper
+// 3 Scissors
+// 0 loss // X
+// 3 draw // Y
+// 6 win  // Z
+
+// 1 paper beats rock // 2 > 1
+// 2 scissors beats paper 3 > 2
+// 3 rock beats scissors 1 > 3
+
+fn rps_round_1 (_round: &str)-> i32 
+{
+    let rps = HashMap::from([
+        ("A X", 3+1),
+        ("A Y", 6+2),
+        ("A Z", 0+3),
+        ("B X", 0+1),
+        ("B Y", 3+2),
+        ("B Z", 6+3),
+        ("C X", 6+1),
+        ("C Y", 0+2),
+        ("C Z", 3+3),
+    ]);
+    return rps[_round];
+}
+
+fn rps_round_2 (_round: &str)-> i32 
+{
+    let rps = HashMap::from([
+        ("A X", 3+0),
+        ("A Y", 1+3),
+        ("A Z", 2+6),
+        ("B X", 1+0),
+        ("B Y", 2+3),
+        ("B Z", 3+6),
+        ("C X", 2+0),
+        ("C Y", 3+3),
+        ("C Z", 1+6),
+    ]);
+    return rps[_round];
+}
+
+pub fn rps_scheduler_1(str: &str) -> i32
+{
+    return str.lines().map(|x| rps_round_1(x)).sum::<i32>(); 
+}
+
+pub fn rps_scheduler_2(str: &str) -> i32
+{
+    return str.lines().map(|x| rps_round_2(x)).sum::<i32>(); 
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn rps_test_1() {
+        let test = "A Y";
+        assert_eq!(8, rps_round_1(test));
+    }
+
+    #[test]
+    fn rps_test_2() {
+        let test = "A Y";
+        assert_eq!(4, rps_round_2(test));
+        assert_eq!(1, rps_round_2("B X"));
+        assert_eq!(7, rps_round_2("C Z"));
+    }
+
+    #[test]
+    fn example_part1() {
+        let test = "A Y\nB X\nC Z";
+        assert_eq!(15, rps_scheduler_1(test));
+    }
+
+    #[test]
+    fn example_part2() {
+        let test = "A Y\nB X\nC Z";
+        assert_eq!(12, rps_scheduler_2(test));
+    }
+}
+
+pub fn day02() {
+
+    let rounds = include_str!("input.txt");
+
+    println!("result est: {}", rps_scheduler_1(rounds)); // 15523
+    println!("result est: {}", rps_scheduler_2(rounds)); // 15702
+}
--- src/main.rs
+++ src/main.rs
@@ -1,46 +1,8 @@
-use std::fs::File;
-use std::io::prelude::*;
-
-// day01
-
-pub fn add_two(a: i32) -> i32 {
-    internal_adder(a, 2)
-}
-
-fn internal_adder(a: i32, b: i32) -> i32 {
-    a + b
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn internal() {
-        assert_eq!(4, internal_adder(2, 2));
-    }
-}
-
-fn day01() {
-    println!("{}", std::env::current_dir().unwrap().display());
-    let file = File::open("src/input.txt");
-    let mut contents = String::new();
-    file.expect("file not found?").read_to_string(&mut contents).unwrap();
-
-    let mut _tmp: Vec::<_> = contents
-        .split("\n\n")
-        .map(|x| x.lines().map(|l| l.parse::<i32>().unwrap())
-        .sum::<i32>()
-        ).collect();
-
-    _tmp.sort();
-    let first = _tmp.last().unwrap();
-    let second = _tmp.iter().rev().take(3).sum::<i32>();
-    
-    println!("max cals: {}", first);
-    println!("top 3 sum: {}", second);
-}
+pub mod day01;
+pub mod day02;
 
 fn main() {
-    day01();
+    day01::day01();
+    day02::day02();
+    
 }
