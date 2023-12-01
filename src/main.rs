35949132d1a6312d4590ece139836a35c42cf444
Rafq77 <sokol-77@wp.pl>
2023-12-01 20:22:09 +0100

    Add year 2023

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,11 @@ use std::io::Error;
 
 mod aoc2020;
 mod aoc2022;
+mod aoc2023;
 
 fn main() -> Result<(), Error> {
     aoc2020::aoc2020()?;
     aoc2022::aoc2022();
+    aoc2023::aoc2023();
     Ok(())
 }
