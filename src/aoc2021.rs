4339627f5744cf7694b02601def419182dad83c8
Rafq77 <sokol-77@wp.pl>
2025-11-04 00:11:10 +0100

    Day03/2021

--- src/aoc2021.rs
+++ src/aoc2021.rs
@@ -2,10 +2,12 @@ use std::io::Error;
 
 mod day01;
 mod day02;
+mod day03;
 
 pub fn aoc2021() -> Result<(), Error> {
     println!("*** YEAR 2021!!!! ***");
     day01::day01()?;
     day02::day02()?;
+    day03::day03()?;
     Ok(())
 }
