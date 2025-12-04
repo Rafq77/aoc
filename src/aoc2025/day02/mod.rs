58c6b79eba9a82e43f771723f1819780b2194f63
Rafq77 <sokol-77@wp.pl>
2025-12-04 01:34:50 +0100

    Fix clippy

--- src/aoc2025/day02/mod.rs
+++ src/aoc2025/day02/mod.rs
@@ -85,6 +85,7 @@ fn check_range(range: (i64, i64), multiple_repetitions: bool) -> Vec<i64> {
 mod tests {
     use super::*;
 
+    #[allow(dead_code)]
     static RANGES: &str = "11-22,95-115,998-1012,1188511880-1188511890,222220-222224,1698522-1698528,446443-446449,38593856-38593862,565653-565659,824824821-824824827,2121212118-2121212124";
 
     #[test]
